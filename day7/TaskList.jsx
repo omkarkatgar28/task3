1991174f4fdbba7f5ce37706140e8df7c95d29b2
omkarkatgar28 <omkatgar45@gmail.com>
2026-09-12 17:24:34 +0530

    Update TaskList.jsx

--- day7/TaskList.jsx
+++ day7/TaskList.jsx
@@ -1,11 +1,12 @@
-import TaskItem from './TaskItem'
+```jsx
+import TaskItem from "./TaskItem";
 
 function TaskList({ tasks, onComplete, onDelete }) {
   return (
     <ul className="mt-6 space-y-3">
       {tasks.map((task, index) => (
         <TaskItem
-          key={index}
+          key={task.id || index}
           task={task}
           index={index}
           onComplete={onComplete}
@@ -13,7 +14,8 @@ function TaskList({ tasks, onComplete, onDelete }) {
         />
       ))}
     </ul>
-  )
+  );
 }
 
-export default TaskList
+export default TaskList;
+```
